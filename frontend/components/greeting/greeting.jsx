42deb5b2953104b96344589ee4bf4999c724c0ff
Martin Stenflo <stenflo@gmail.com>
2020-04-12 14:10:23 -0700

    fix greeting for logged in user in profile

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -1,28 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-const Greeting = ({ currentUser, logout }) => {
-  const sessionLinks = () => (
-    <nav className="site-header-right">
-      <Link to="/login">Login</Link>
-      &nbsp; | &nbsp;
-      <Link to="/signup">Sign Up</Link>
-    </nav>
-  );
-  const personalGreeting = () => (
-    <div>
-      <ul className="site-header-right">
-        <Link to={`/member/${currentUser.username}`}>
-          <li>{currentUser.username}</li>
-          <li><img src={window.avatarIconURL} /></li>
-        </Link>
-        <button className="header-button" onClick={logout}>Log Out</button>
-      </ul>
-    </div>
-  );
-
-  return currentUser ? personalGreeting() : sessionLinks();
-};
-
+class Greeting extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: this.props.currentUser,
+      logout: this.props.logout
+    }
+  }
+  
+  render() {
+    if (!this.state.currentUser) {
+      return (
+        <div>
+          <nav className="site-header-right">
+            <Link to="/login">Login</Link>
+            &nbsp; | &nbsp;
+            <Link to="/signup">Sign Up</Link>
+          </nav>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <ul className="site-header-right">
+            <Link to={`/member/${this.state.currentUser.username}`}>
+              <li>{this.state.currentUser.username}</li>
+              <li><img src={window.avatarIconURL} /></li>
+            </Link>
+            <button className="header-button" onClick={this.props.logout}>Log Out</button>
+          </ul>
+        </div>
+      )
+    }
+  }
+}
 
 export default Greeting;

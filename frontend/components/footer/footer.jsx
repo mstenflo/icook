91476029614aa22c7af0a672f42ad1eaadaddf5e
Martin Stenflo <stenflo@gmail.com>
2020-05-11 14:23:36 -0700

    add font awesome icons in footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -9,16 +9,20 @@ class Footer extends Component {
         <div>
           <h1>Categories</h1>
           <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/recipes">Recipes</Link></li>
-            <li><Link to="/publish">Create Recipe</Link></li>
+            <li><Link to="/"><i className="fas fa-home"></i> Home</Link></li>
+            <li><Link to="/recipes"><i className="fas fa-utensils"></i> Recipes</Link></li>
+            <li><Link to="/publish"><i className="fas fa-pencil-alt"></i> Create Recipe</Link></li>
           </ul>
         </div>
-        <div>
+        <div className="social">
           <h1>About Me</h1>
           <ul>
-            <li><a href="https://www.linkedin.com/in/martin-stenflo-3140251a1/">LinkedIn</a></li>
-            <li><a href="https://github.com/mstenflo">Github</a></li>
+            <li>
+              <a href="https://www.linkedin.com/in/martin-stenflo-3140251a1/"><i className="fab fa-linkedin fa-3x"></i></a>
+              <a href="https://github.com/mstenflo"><i className="fab fa-github fa-3x"></i></a>
+              <a href="https://angel.co/u/martin-stenflo"><i className="fab fa-angellist fa-3x"></i></a>
+            </li>
+            <li><a href="https://martin.stenflo.org">martin.stenflo.org</a></li>
           </ul>
         </div>
       </div>
